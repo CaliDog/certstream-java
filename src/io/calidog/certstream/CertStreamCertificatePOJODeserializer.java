73a0a124ea5dc2114f81491cafb3c414627b6674
nitram22 <martin-wilhelm-97@web.de>
2020-07-09 14:22:31 +0200

    Fix parsing error when extensions contain "extra" field

--- src/io/calidog/certstream/CertStreamCertificatePOJODeserializer.java
+++ src/io/calidog/certstream/CertStreamCertificatePOJODeserializer.java
@@ -4,6 +4,7 @@ import com.google.gson.*;
 
 import java.lang.reflect.Type;
 import java.util.HashMap;
+import java.util.Map;
 
 public class CertStreamCertificatePOJODeserializer implements JsonDeserializer<CertStreamCertificatePOJO> {
 
@@ -13,50 +14,47 @@ public class CertStreamCertificatePOJODeserializer implements JsonDeserializer<C
             Type type,
             JsonDeserializationContext jsonDeserializationContext
     ) throws JsonParseException {
+
         JsonObject jsonObj = jsonElement.getAsJsonObject();
+        JsonObject jsonExtensions = new JsonObject();
 
-        JsonObject jsonExtensions;
-
-        HashMap<String, String[]> extensionMap = null;
-
+        // remove extensions from json object
         if (jsonObj.has("extensions"))
         {
             jsonExtensions = jsonObj.remove("extensions").getAsJsonObject();
-
-            final HashMap<String, String[]> finalExtensionMap = new HashMap<>(jsonExtensions.size());
-
-            jsonExtensions
-                    .entrySet()
-                    .forEach
-                    (
-                            (java.util.Map.Entry<String, JsonElement> entry) ->
-                            {
-                                String key = entry.getKey();
-                                JsonElement value = entry.getValue();
-                                String[] extensionValueList;
-                                try {
-                                    extensionValueList = new String[] { value.getAsString() };
-                                } catch (IllegalStateException | UnsupportedOperationException e) {
-                                    JsonArray extensionJsonArray = value.getAsJsonArray();
-                                    extensionValueList = new String[extensionJsonArray.size()];
-
-                                    for (int i = 0; i < extensionJsonArray.size(); i++) {
-                                        extensionValueList[i] = extensionJsonArray.get(i).getAsString();
-                                    }
-                                }
-
-                                finalExtensionMap.put(key, extensionValueList);
-                            }
-                    );
-
-            extensionMap = finalExtensionMap;
         }
 
-        CertStreamCertificatePOJO retVal =
-                jsonDeserializationContext.deserialize(jsonElement, type);
+        // parse entry normally
+        CertStreamCertificatePOJO retVal = new Gson().fromJson(jsonElement, CertStreamCertificatePOJO.class);
 
-        retVal.extensions = extensionMap;
+        // parse extensions externally
+        retVal.extensions = deserializeExtension(jsonExtensions);
 
         return retVal;
+    }
+
+    private HashMap<String, String[]> deserializeExtension(JsonObject extensions){
+        final HashMap<String, String[]> finalMap = new HashMap<>(extensions.size());
+
+        extensions.entrySet().forEach((Map.Entry<String, JsonElement> entry) -> {
+            String key = entry.getKey();
+            JsonElement value = entry.getValue();
+            String[] extensionValues;
+
+            try {
+                extensionValues = new String[] { value.getAsString() };
+            } catch (IllegalStateException | UnsupportedOperationException e) {
+                JsonArray extensionJsonArray = value.getAsJsonArray();
+                extensionValues = new String[extensionJsonArray.size()];
+
+                for (int i = 0; i < extensionJsonArray.size(); i++) {
+                    extensionValues[i] = extensionJsonArray.get(i).getAsString();
+                }
+            }
+
+            finalMap.put(key, extensionValues);
+        });
+
+        return finalMap;
     }
 }
